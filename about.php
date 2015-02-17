1fee38a6d923dfd26be5be059fc799fe0bb8717b
Beatrice <beatrice.salim@stu.aii.edu>
2015-02-16 20:26:17 -0800

    add images about.php

--- about.php
+++ about.php
@@ -14,21 +14,21 @@
     
     <div id="meet_pastors">
     	<div id="pastors">
-        <img src="">
+        <img src="images/coleman.JPG">
         <h3> Sheila Schuller Coleman </h3>
         <h4> Senior Pastor </h4>
         <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. </p>
         </div>
         
         <div id="pastors">
-        <img src="">
+        <img src="images/shaw.JPG">
         <h3> Harold Shaw </h3>
         <h4> Pastor: Hope 2 Go Mission </h4>
         <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. </p>
         </div>
         
         <div id="pastors">
-        <img src="">
+        <img src="images/penner.JPG">
         <h3> James Penner </h3>
         <h4> Pastor </h4>
         <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. </p>
